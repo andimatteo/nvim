2d1004aa8f14dc3469c026d5f95f8f549765fda9
andrea <a.dimatteo5@studenti.unipi.it>
2025-04-26 01:09:37 +0200

    packer compiled modified

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -140,6 +140,11 @@ _G.packer_plugins = {
     path = "/Users/andrea/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
+  ["vim-be-good"] = {
+    loaded = true,
+    path = "/Users/andrea/.local/share/nvim/site/pack/packer/start/vim-be-good",
+    url = "https://github.com/ThePrimeagen/vim-be-good"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/Users/andrea/.local/share/nvim/site/pack/packer/start/vim-fugitive",
